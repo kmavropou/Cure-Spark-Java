28c376a3ccbbcf9effa2b35fcf6c83a8e468a374
Kyriaki Mavropoulou <mavropoul@csd.auth.gr>
2019-06-20 23:22:51 +0300

    Python Hier.
    Python script

---
+++ test1cure.py
@@ -0,0 +1,40 @@
+from sklearn.cluster import AgglomerativeClustering
+import pandas as pd
+
+def distclust(file, c):
+    filefinal=open(file,"r")
+    listofpoints= []
+    for line in filefinal.readlines():
+        line=line[1:-2]
+        spliting=line.split(",")
+        splitfloat=[]
+        for s in spliting:
+            splitfloat.append(float(s))
+
+        listofpoints.append(splitfloat)
+
+
+
+    x=[]
+    y=[]
+    for point in listofpoints:
+        x.append(point[0])
+        y.append(point[1])
+
+    model = AgglomerativeClustering(n_clusters=c,affinity="euclidean",linkage ="single")
+    predictions = model.fit_predict(listofpoints)
+    data={'x':x,'y':y,'Predictions':predictions}
+
+    results=pd.DataFrame(data)
+    print(results.head(10))
+
+    results.to_csv('predictions.csv')
+
+
+distclust("resultsjava",5)
+
+
+
+
+
+
